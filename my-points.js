5043b0a0ee0de03f418b77369dddb2bf4138b738
briadonohue <bcd9694@nyu.edu>
2023-04-13 19:17:01 -0400

    test1

---
+++ my-points.js
@@ -0,0 +1,50 @@
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "Trafalgar Square"
+      },
+      "geometry": {
+        "coordinates": [
+          -0.13101028904233658,
+          51.507641545381944
+        ],
+        "type": "Point"
+      },
+      "id": 0
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "coordinates": [
+          [
+            -0.13033117685699835,
+            51.50687734162045
+          ],
+          [
+            -0.15943675680213687,
+            51.51329771994611
+          ]
+        ],
+        "type": "LineString"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "location": "Hotel"
+      },
+      "geometry": {
+        "coordinates": [
+          -0.159448399703507,
+          51.51339096987948
+        ],
+        "type": "Point"
+      },
+      "id": 2
+    }
+  ]
+}
